9d74123c077c84e7a3b354265e3b9f498a3fa37d
Oleksandr <fjvffr@gmail.com>
2024-06-16 23:17:21 +0300

    add input

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import useTelegram from '../hooks/useTelegram'
 
-export default function () {
-  return (
-    <div>
-        Form
-    </div>
-  )
+export default function Form() {
+    const [text, setText] = useState('')
+    const {tg} = useTelegram()
+    useEffect(()=> {
+        tg.MainButton.setParams({
+            text: 'Send data'
+        })
+    },[])
+
+    useEffect(() => {
+        if (!text) {
+            tg.MainButton.hide()
+        } else {
+            tg.MainButton.show()
+        }
+    }, [text])
+
+    const onChangeText = (e) => {
+        setText(e.target.value)
+    }
+    return (
+        <div>
+            <input className={'input'}
+                type='text'
+                placeholder='Type here'
+                value={text}
+                onChange={onChangeText} />
+        </div>
+    )
 }
